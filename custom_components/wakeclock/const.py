581e9c1665cb0b08d91c1484b247e5c547e5a6a0
reiniertc <reinier@taudin-chabot.nl>
2025-12-16 20:17:09 +0100

    Create const.py

---
+++ custom_components/wakeclock/const.py
@@ -0,0 +1,15 @@
+DOMAIN = "wakeclock"
+
+CONF_SNOOZE_MINUTES = "snooze_minutes"
+DEFAULT_SNOOZE_MINUTES = 9
+
+WEEKDAYS = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+ATTR_MAP = {
+    "mon": "maandag",
+    "tue": "dinsdag",
+    "wed": "woensdag",
+    "thu": "donderdag",
+    "fri": "vrijdag",
+    "sat": "zaterdag",
+    "sun": "zondag",
+}
