bc8d756215a95d48a3ba840d7955a7f431b1df7e
reiniertc <reinier@taudin-chabot.nl>
2025-12-16 22:39:29 +0100

    Create const.py

---
+++ custom_components/wakeclock/const.py
@@ -0,0 +1,11 @@
+DOMAIN = "wakeclock"
+
+STORE_KEY = "wakeclock_state"
+STORE_VERSION = 1
+
+ENTITY_ID = "switch.wakeclock"
+
+ATTR_SNOOZETIME = "snoozetime"
+ATTR_NEXTALARM = "nextalarm"
+
+DAYS = ["ma", "di", "wo", "do", "vr", "za", "zo"]
